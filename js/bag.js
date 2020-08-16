1d1fe22513317e7c9b66aa900804570076634f41
BatoAlvin <alvinbato112@gmail.com>
2020-08-16 11:57:51 +0300

    responsive website

---
+++ js/bag.js
@@ -0,0 +1,69 @@
+    //Opens a side menue to links
+    function openSlideMenu() {
+        document.getElementById('side-menu').style.width = '250px';
+        document.getElementById('main').style.marginLeft = '250px';
+      }
+      function closeSlideMenu() {
+        document.getElementById('side-menu').style.width = '0';
+        document.getElementById('main').style.marginLeft = '0';
+      }
+
+      let btn = document.getElementById('bat');
+      btn.addEventListener('click', ()=>{
+        btn.textContent = 'Ordered';
+      });
+
+      let btn1 = document.getElementById('bat1');
+      btn1.addEventListener('click', ()=>{
+        btn1.textContent = 'Ordered';
+      });
+
+      let btn2 = document.getElementById('bat2');
+      btn2.addEventListener('click', ()=>{
+        btn2.textContent = 'Ordered';
+      });
+
+      let btn3 = document.getElementById('bat3');
+      btn3.addEventListener('click', ()=>{
+        btn3.textContent = 'Ordered';
+      });
+
+      let btn4 = document.getElementById('bat4');
+      btn4.addEventListener('click', ()=>{
+        btn4.textContent = 'Ordered';
+      });
+
+      let btn5 = document.getElementById('bat5');
+      btn5.addEventListener('click', ()=>{
+        btn5.textContent = 'Ordered';
+      });
+
+      let btn6 = document.getElementById('bat6');
+      btn6.addEventListener('click', ()=>{
+        btn6.textContent = 'Ordered';
+      });
+
+      let btn7 = document.getElementById('bat7');
+      btn7.addEventListener('click', ()=>{
+        btn7.textContent = 'Ordered';
+      });
+
+      let btn8 = document.getElementById('bat8');
+      btn8.addEventListener('click', ()=>{
+        btn8.textContent = 'Ordered';
+      });
+
+      let btn9 = document.getElementById('bat9');
+      btn9.addEventListener('click', ()=>{
+        btn9.textContent = 'Ordered';
+      });
+
+      let btn10 = document.getElementById('bat10');
+      btn10.addEventListener('click', ()=>{
+        btn10.textContent = 'Ordered';
+      });
+
+      let btn11 = document.getElementById('bat11');
+      btn11.addEventListener('click', ()=>{
+        btn11.textContent = 'Ordered';
+      });
